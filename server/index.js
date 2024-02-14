1005d644f2342a384d1e3f579f6cc60e57d9b9d6
fehervaryy <martindmhe@gmail.com>
2024-02-14 15:07:30 -0500

    added POST method

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const port = 3000;
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     console.log("Working")
     res.sendStatus(200)
 })
+
+app.post('/post', (req, res) => {
+    const body = req.body
+    const { text } = body;
+    console.log(text)
+    // const reqText = body.text
+    // console.log(`Received text: ` + reqText)
+    res.sendStatus(200)
+} )
 
 app.listen(port, () => {
     console.log("Server running")
--- src/components/TestFetch.jsx
+++ src/components/TestFetch.jsx
@@ -1,16 +1,41 @@
 import { useState, useEffect } from "react";
+import '../index.css'
 
 const TestFetch = () => {
 
     const [ fetchedData, setFetchedData ] = useState();
+    const [ postBody, setPostBody] = useState("");
 
     return ( 
         <div>
-            <button onClick={ async () => {
-                await fetch('http://localhost:3000/')}
-             }>
-                GET REQUEST
-            </button>
+            <div className="rest-container">
+                <div className="get">
+                    <button onClick={ async () => {
+                    await fetch('http://localhost:3000/')}
+                    }>
+                    GET REQUEST
+                    </button>
+                </div>
+                <div className="post">
+                    <button onClick={ async () => {
+                    await fetch('http://localhost:3000/post', {
+                        method : 'POST',
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                          },
+                        body: JSON.stringify({
+                            "text" : postBody
+                        })
+                    })
+                    }}>POST</button>
+                    <input type="text" onChange={e => {
+                        setPostBody(e.target.value)
+                    }} />
+                </div>
+            </div>
+            
+            
             
         </div>
      );
